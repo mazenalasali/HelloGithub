649a0e54e2526cef3e51d7fb44939a1c4c7bed57
mazenalasali <mazenalasali@gmail.com>
2020-11-26 23:38:33 +0200

    Fixing Spaces

--- HelloGithubWithJava.java
+++ HelloGithubWithJava.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 class HelloGithubWithJava{  
         public static void main(String args[]){  
         	Scanner input = new Scanner(System.in);	
-        	System.out.print("What Is Your Name? ");
+        	System.out.print("What Is Your Name ? ");
         	String name = input.next();
-            System.out.println("Hello Github With Java By: " + name);  
+        	System.out.println("Hello Github With Java By: " + name);  
         }  
     }  
